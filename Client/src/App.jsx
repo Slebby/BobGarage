c3f19ca5b9654fcc6cd3408f03bae76dc1efc946
Slebby <109596501+Slebby@users.noreply.github.com>
2023-09-18 14:55:58 +1000

    Link the css

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -6,7 +6,7 @@ import Footer from './components/layout/Footer';
 function App() {
   return (
     <Router>
-      <Header/>
+      <Header branding='Bob Garage'/>
       <Routes>
         <Route></Route>
       </Routes>
