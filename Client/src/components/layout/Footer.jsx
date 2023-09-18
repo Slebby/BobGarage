0b2d3070c61ae39aa3df72aa44a4cb7de019591b
Slebby <109596501+Slebby@users.noreply.github.com>
2023-09-18 16:00:56 +1000

    Update App.css, Header.jsx, Footer.jsx
    Make Navbar item stay on the right and change the padding on footer

--- Client/src/components/layout/Footer.jsx
+++ Client/src/components/layout/Footer.jsx
@@ -3,8 +3,8 @@
 const Footer = props => {
     const date = new Date();
   return (
-    <footer className="mt-auto nav-bg-color">
-        <p className="text-end text-white mx-4 mb-0">copyright {String.fromCharCode(169)} {date.getFullYear()} Websites &apos;r&apos; us</p>
+    <footer className="mt-auto main-bg-color">
+        <p className="text-end text-white mx-4 mb-0 py-3">copyright {String.fromCharCode(169)} {date.getFullYear()} Websites &apos;r&apos; us</p>
     </footer>
   )
 }
--- Client/src/components/layout/Header.jsx
+++ Client/src/components/layout/Header.jsx
@@ -5,14 +5,14 @@ import { Link } from 'react-router-dom'
 const Header = props => {
   return (
     <header>
-        <nav className="navbar navbar-expand-lg nav-bg-color" data-bs-theme="dark">
+        <nav className="navbar navbar-expand-lg main-bg-color" data-bs-theme="dark">
             <div className="container-fluid">
                 <Link className="navbar-brand" to="/">{props.branding}</Link>
                 <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
                     <span className="navbar-toggler-icon"></span>
                 </button>
                 <div className="collapse navbar-collapse" id="navbarText">
-                    <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+                    <ul className="navbar-nav ms-auto mb-2 mb-lg-0">
                         <li className="nav-item">
                             <Link className="nav-link" aria-current="page" to="#">Services</Link>
                         </li>
