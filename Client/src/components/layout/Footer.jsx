72adcd311872d6689959d415934ae169bab9fab8
Slebby <109596501+Slebby@users.noreply.github.com>
2023-09-18 16:23:31 +1000

    Update Footer.jsx
    add margin top

--- Client/src/components/layout/Footer.jsx
+++ Client/src/components/layout/Footer.jsx
@@ -4,7 +4,7 @@ const Footer = props => {
     const date = new Date();
   return (
     <footer className="mt-auto main-bg-color">
-        <p className="text-end text-white mx-4 mb-0 py-3">copyright {String.fromCharCode(169)} {date.getFullYear()} Websites &apos;r&apos; us</p>
+        <p className="text-end text-white mx-4 mb-0 py-3 mt-3">copyright {String.fromCharCode(169)} {date.getFullYear()} Websites &apos;r&apos; us</p>
     </footer>
   )
 }
