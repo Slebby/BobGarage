ab18d024832bda6ea9f917c7e9258924efd4c982
Slebby <109596501+Slebby@users.noreply.github.com>
2023-09-18 16:33:51 +1000

    Add 2 pages
    Add About page and home page

---
+++ Client/src/components/pages/About.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+const About = props => {
+  return (
+    <div className="container my-3 ">
+        <h2 className="text-center fw-bold">About Us</h2>
+        <p>Bob's garage is not just a one-man show; it's a collaborative effort with his dedicated team. Together, they form a tight-knit group of automotive enthusiasts who are passionate about their craft and dedicated to preserving the legacy of classic cars.</p>
+        <p>Bob's team is a diverse bunch, each member bringing their unique skills and expertise to the table. There's Dave, the master mechanic, whose hands seem to have an almost magical touch when it comes to fixing engines and getting them purring like kittens. Then there's Sarah, the detail-oriented bodywork specialist, who can take a rusted hulk and turn it into a shining work of art. And let's not forget Mike, the electrical whiz, who can diagnose and solve the trickiest wiring issues with ease.</p>
+        <p>Together, they tackle projects big and small. Whether it's a full restoration of a vintage Mustang or a simple oil change for a neighbor's sedan, Bob and his team approach every task with the same level of enthusiasm and dedication. They work in harmony, each member knowing their role and contributing their expertise to the project at hand.</p>
+        <p>But it's not all work and no play in Bob's garage. The team also shares a deep camaraderie, often gathering for barbecues, late-night discussions about cars, and the occasional friendly competition on the garage's vintage arcade machine. They celebrate each other's successes and support one another through the inevitable challenges that come with restoring classic cars.</p>
+        <p>Bob's garage is more than just a place for car repairs; it's a hub of teamwork and collaboration. The bond between Bob and his team is palpable, and it's a testament to the power of shared passion and a common goal. Together, they keep the spirit of classic cars alive and ensure that Bob's garage remains a welcoming haven for automotive enthusiasts for years to come.</p>
+    </div>
+  )
+}
+
+About.propTypes = {}
+
+export default About
---
+++ Client/src/components/pages/Home.jsx
@@ -0,0 +1,41 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
+
+const Home = props => {
+  return (
+    <main>
+        <section className="secondary-bg-color">
+            <p className="text-center py-5 fs-2 mb-0 fw-semibold">Welcome</p>
+        </section>
+        <section>
+            <div className="container text-center">
+                <h3 className="m-4">Check our Services</h3>
+                <div className="row row-cols-2 gap-5 justify-content-center">
+                    <div className="card col-md-5">
+                        <img src="#" alt="Picture" className="card-img-top"/>
+                        <div className="card-body">
+                            <h5 className="card-title">Mechanical Repairs and Maintenance</h5>
+                            <p className="card-text">Bob's team can handle everything from routine oil changes and brake inspections to complex engine repairs. They ensure that your vehicle runs smoothly and efficiently.</p>
+                            <Link className="btn btn-secondary" to="/">Explore</Link>
+                        </div>
+                    </div>
+                    <div className="card col-md-5">
+                        <img src="#" alt="Picture" className="card-img-top"/>
+                        <div className="card-body">
+                            <h5 className="card-title">Tire Services</h5>
+                            <p className="card-text">From tire rotations and balancing to tire replacements, Bob's team ensures that your vehicle's tires are in top condition, maximizing safety and performance.</p>
+                            <Link className="btn btn-secondary" to="/">Explore</Link>
+                        </div>
+                    </div>
+
+                </div>
+            </div>
+        </section>
+    </main>
+  )
+}
+
+Home.propTypes = {}
+
+export default Home
