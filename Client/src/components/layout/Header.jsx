37219fb8b274793ffddca950e9905a2cea63b1e3
Slebby <109596501+Slebby@users.noreply.github.com>
2023-09-21 20:35:54 +1000

    Add favicons and logo

--- Client/src/components/layout/Header.jsx
+++ Client/src/components/layout/Header.jsx
@@ -9,7 +9,10 @@ const Header = props => {
     <header>
         <nav className="navbar navbar-expand-lg main-bg-color" data-bs-theme="dark">
             <div className="container-fluid">
-                <Link className="navbar-brand" to="/">{props.branding}</Link>
+                <Link className="navbar-brand" to="/">
+                    <img src="BobGarageLogo.png" alt="Bob Garage Logo" className="d-inline-block align-text-top" style={{width: "40%"}}/>
+                    {/* {props.branding} */}
+                </Link>
                 <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
                     <span className="navbar-toggler-icon"></span>
                 </button>
