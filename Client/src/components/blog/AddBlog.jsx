48ad1e8104dfbd51c55a6cb8ec65aa5a7576ee62
Slebby <109596501+Slebby@users.noreply.github.com>
2023-09-23 16:26:26 +1000

    Add State to Addblog.jsx

--- Client/src/components/blog/AddBlog.jsx
+++ Client/src/components/blog/AddBlog.jsx
@@ -1,8 +1,41 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import { FaAnglesLeft } from 'react-icons/fa6';
 
-const AddBlog = () => {
+const AddBlog = (props) => {
+  const [formData, setFormData] = useState({
+    blogId: '1',
+    blogHeader: '',
+    blogTitle: '',
+    blogBody: '',
+    errors: {}
+  });
+
+  const {blogId, blogHeader, blogTitle, blogBody} = formData;
+
+  const blogOnChange = e => {
+    // console.log(e);
+
+    setFormData({
+      ...formData, [e.target.name]: e.target.value
+    });
+  };
+
+  const blogOnSubmit = (e) => {
+    e.preventDefault();
+
+    console.log('Add Blog - Submitting Form...');
+
+    const newBlog = {
+      blogId,
+      blogHeader,
+      blogTitle,
+      blogBody,
+    }
+
+    console.log(newBlog);
+  };
+  
   return (
     <div className="container mb-5">
         <h3 className="text-center m-4 fw-semibold">Post Blog</h3>
@@ -12,10 +45,11 @@ const AddBlog = () => {
             </Link>
         </p>
         <section className="card secondary-bg-color border-0">
-            <form>
+            <form onSubmit={e => blogOnSubmit(e)}>
                 <header className="card-header header-bg-color border-bottom-0">
                     <div className="form-floating">
-                        <input type="text" name="header" placeholder="Header Text Here" id="floatingHeader" className="form-control" />
+                        <input type="text" name="blogHeader" placeholder="Header Text Here" id="floatingHeader" className="form-control"
+                        onChange={e => blogOnChange(e)} />
                         <label htmlFor="floatingHeader" className="opacity-75">Header</label>
                     </div>
                 </header>
@@ -23,13 +57,15 @@ const AddBlog = () => {
                 <div className="card-body">
                     <div className="card-title">
                         <div className="form-floating">
-                            <input type="text" name="title" id="floatingTitle" placeholder="Title Text Here" className="form-control" />
+                            <input type="text" name="blogTitle" id="floatingTitle" placeholder="Title Text Here" className="form-control"
+                            onChange={e => blogOnChange(e)} />
                             <label htmlFor="floatingTitle" className="opacity-75">Title</label>
                         </div>
                     </div>
                     <div className="card-text">
                         <div className="form-floating">
-                            <textarea type="text" name="blogBody" id="floatingText" placeholder="Body Text Here" className="form-control" style={{height: "20rem"}}/>
+                            <textarea type="text" name="blogBody" id="floatingText" placeholder="Body Text Here" className="form-control" style={{height: "20rem"}}
+                            onChange={e => blogOnChange(e)}/>
                             <label htmlFor="floatingText" className="opacity-75">Body</label>
                         </div>
                     </div>
