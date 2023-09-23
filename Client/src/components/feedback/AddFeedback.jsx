6342cd0366c7a77d02dce1d9eb996fa1f6ae2fe1
Slebby <109596501+Slebby@users.noreply.github.com>
2023-09-23 16:14:18 +1000

    Add State to the AddFeedback.jsx

--- Client/src/components/feedback/AddFeedback.jsx
+++ Client/src/components/feedback/AddFeedback.jsx
@@ -1,8 +1,39 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import { FaAnglesLeft } from 'react-icons/fa6';
 
-const AddFeedback = () => {
+const AddFeedback = (props) => {
+  const [formData, setFormData] = useState({
+    feedbackId: '1',
+    feedbackHeader: '',
+    feedbackBody: '',
+    errors: {}
+  });
+
+  const { feedbackId, feedbackHeader, feedbackBody } = formData;
+
+  const feedbackOnChange = (e) => {
+    // console.log(e);
+
+    setFormData({
+        ...formData, [e.target.name]: e.target.value
+    });
+  };
+
+  const feedbackOnSubmit = (e) => {
+    e.preventDefault();
+
+    console.log('Add feedback - Submit running');
+
+    const newFeedback = {
+      feedbackId,
+      feedbackHeader,
+      feedbackBody
+    };
+
+    console.log(newFeedback)
+  };
+
   return (
     <div className="container mb-5">
         <h3 className="text-center m-4 fw-semibold">Add Feedback</h3>
@@ -12,10 +43,15 @@ const AddFeedback = () => {
             </Link>
         </p>
         <section className="card secondary-bg-color border-0">
-            <form>
+            <form onSubmit={e => feedbackOnSubmit(e)}>
                 <header className="card-header header-bg-color border-bottom-0">
                     <div className="form-floating">
-                        <input type="text" name="header" placeholder="Header Text Here" id="floatingHeader" className="form-control" />
+                        <input
+                        type="text"
+                        name="feedbackHeader"
+                        placeholder="Header Text Here" id="floatingHeader"
+                        className="form-control"
+                        onChange={e => feedbackOnChange(e)} />
                         <label htmlFor="floatingHeader" className="opacity-75">Header</label>
                     </div>
                 </header>
@@ -23,7 +59,14 @@ const AddFeedback = () => {
                 <div className="card-body">
                     <div className="card-text">
                         <div className="form-floating">
-                            <textarea type="text" name="feedbackBody" id="floatingText" placeholder="Body Text Here" className="form-control" style={{height: "20rem"}}/>
+                            <textarea
+                            type="text"
+                            name="feedbackBody"
+                            id="floatingText"
+                            placeholder="Body Text Here"
+                            className="form-control"
+                            style={{height: "20rem"}}
+                            onChange={e => feedbackOnChange(e)} />
                             <label htmlFor="floatingText" className="opacity-75">Body</label>
                         </div>
                     </div>
