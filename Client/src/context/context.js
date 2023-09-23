effd89670bdf64075d856b2d4fcb0a6bed6c4298
Slebby <109596501+Slebby@users.noreply.github.com>
2023-09-23 16:48:41 +1000

    add Context that hold action and state

---
+++ Client/src/context/context.js
@@ -0,0 +1,52 @@
+import React, { Component } from 'react';
+import axios from 'axios';
+
+const Context = React.createContext();
+const reducer = (state, action) => {
+    switch(action.type){
+        case "DELETE_FEEDBACK":
+            return{
+                ...state,
+                feedbackList: state.feedbackList.filter(
+                    item => item.feedbackId !== action.payload
+                )
+            };
+        case "UPDATE_FEEDBACK":
+            return{
+                ...state,
+                feedbackList: state.feedbackList.map(
+                    item => item.feedbackId === action.payload.id ? (item = action.payload) : item
+                )
+            };
+        case "ADD_FEEDBACK":
+            return{
+                ...state,
+                feedbackList: [action.payload, ...state.feedbackList]
+            };
+        default:
+            return state;
+    }
+}
+
+export class Provider extends Component{
+    async componentDidMount(){
+        const res = await axios.get('https://jsonplaceholder.typicode.com/comments');
+        console.log(res.data);
+        this.setState({ feedbackList: res.data });
+    };
+
+    state = {
+        feedbackList: [],
+        dispatch: action => {
+            this.setState(state => reducer(state, action));
+        }
+    }
+
+    render(){
+        return(
+            <Context.Provider value={this.state}>
+                {this.props.children}
+            </Context.Provider>
+        )
+    }
+}
