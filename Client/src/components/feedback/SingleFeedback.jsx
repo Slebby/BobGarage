813dda5013b16088d921febc8097719b44076a07
Slebby <109596501+Slebby@users.noreply.github.com>
2023-09-25 02:46:28 +1000

    Add Delete function

--- Client/src/components/feedback/SingleFeedback.jsx
+++ Client/src/components/feedback/SingleFeedback.jsx
@@ -5,6 +5,15 @@ import axios from 'axios';
 
 const SingleFeedback = (props) => {
   const { feedId, feedbackBody, feedbackTitle } = props.feedback;
+
+  const feedbackOnDelete = (id, dispatch) => {
+    console.log('Deleted Clicked!');
+    console.log(`Id: ${id}`);
+
+    axios.delete(`/api/feedback/delete/${id}`);
+    dispatch({ type: 'DELETE_FEEDBACK', payload: id});
+  };
+  
   return (
     <Consumer>
         { value => { const { dispatch } = value;
@@ -15,7 +24,7 @@ const SingleFeedback = (props) => {
                         <h5 className="card-title">{feedbackTitle}</h5>
                         <p className="card-text">{feedbackBody}</p>
                         <Link className="btn btn-secondary me-3" to={`./edit/${feedId}`}>Edit</Link>
-                        <Link className="btn btn-danger ms-3">Delete</Link>
+                        <Link className="btn btn-danger ms-3" onClick={() => {feedbackOnDelete(feedId, dispatch)}}>Delete</Link>
                     </div>
                 </div>
             )
