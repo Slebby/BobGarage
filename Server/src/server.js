10ae7a577d167d853167dda0fa5212275c757235
Slebby <109596501+Slebby@users.noreply.github.com>
2023-09-24 15:01:08 +1000

    Add and testing the http methods

---
+++ Server/src/server.js
@@ -0,0 +1,40 @@
+const express = require('express');
+
+const app = express();
+
+app.use(express.json());
+
+app.use(express.urlencoded({ extended: true }));
+
+// Feedback Routes.
+app.get('/api/feedback', (req, res) => {
+    console.log('/api/feedback - GET');
+
+    res.send('Get All Feedback, GET REQUEST');
+});
+
+app.get('/api/feedback/edit/:id', (req, res) => {
+    console.log('/api/feedback/edit/:id - GET');
+
+    res.send('Get One Feedback, GET REQUEST');
+});
+
+app.post('/api/feedback/add', (req, res) => {
+    console.log('/api/feedback/add - POST')
+    res.send('Add Feedback - POST REQUEST');
+});
+
+app.put('/api/feedback/edit/:id', (req, res) => {
+    console.log('/api/feedback/edit/:id - PUT');
+
+    res.send('/api/feedback/edit/:id - PUT REQUEST')
+});
+
+app.delete('/api/feedback/delete/:id', (req, res) => {
+    console.log('/api/feedback/delete/:id - DELETE');
+    
+    res.send('/api/feedback/delete/:id - DELETE REQUEST');
+});
+
+const PORT = process.env.PORT || 3060;
+app.listen(PORT, () => console.log(`Server is running on port: ${PORT}`));
