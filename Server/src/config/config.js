8276be4cea607f49ea555030e9c77c020d35b57f
Slebby <109596501+Slebby@users.noreply.github.com>
2023-09-24 15:33:02 +1000

    Import .env file and setup

--- Server/src/config/config.js
+++ Server/src/config/config.js
@@ -1,15 +1,16 @@
 // File: src/config/config.js
 // This file will hold all the config for our server.
+require('dotenv').config();
 
 module.exports = {
-    port: process.env.PORT || 3060,
+    port: process.env.PORT,
     db: {
-        database: process.env.DB_NAME || 'bobgarage',
-        user: process.env.DB_USER || 'root',
-        password: process.env.DB_PASS || 'root',
+        database: process.env.DB_NAME,
+        user: process.env.DB_USER,
+        password: process.env.DB_PASS,
         options: {
-            dialect: process.env.DIALECT || 'sqlite',
-            host: process.env.HOST || 'localhost',
+            dialect: process.env.DIALECT,
+            host: process.env.HOST,
             storage: './data/bobgarage.sqlite',
         }
     },
