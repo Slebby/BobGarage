8479061c519d044d466bf25cbcef9955e53b2f62
Slebby <109596501+Slebby@users.noreply.github.com>
2023-09-25 15:48:23 +1000

    Change the button color

--- Client/src/components/feedback/SingleFeedback.jsx
+++ Client/src/components/feedback/SingleFeedback.jsx
@@ -23,7 +23,7 @@ const SingleFeedback = (props) => {
                     <div className="card-body">
                         <h5 className="card-title">{feedbackTitle}</h5>
                         <p className="card-text">{feedbackBody}</p>
-                        <Link className="btn btn-secondary me-3 fw-semibold" to={`./edit/${feedId}`}>Edit</Link>
+                        <Link className="btn main-bg-color btn-color me-3 fw-semibold" to={`./edit/${feedId}`}>Edit</Link>
                         <Link className="btn btn-danger ms-3 fw-semibold" onClick={() => {feedbackOnDelete(feedId, dispatch)}}>Delete</Link>
                     </div>
                 </div>
