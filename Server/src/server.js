8be392048fbc17d84b1cb0ae8e2a216074430a69
Slebby <109596501+Slebby@users.noreply.github.com>
2023-09-25 15:14:07 +1000

    Add Blog Routes

--- Server/src/server.js
+++ Server/src/server.js
@@ -81,11 +81,86 @@ app.delete('/api/feedback/delete/:id', async (req, res) => {
         }
     });
     if (deleteFeedback == 1){
-        console.log(`Update status: ${deleteFeedback}`);
+        console.log('Feedback Update failed');
         res.send('Feedback has been updated!');
     } else {
-        console.log(`Update status: ${deleteFeedback}`);
+        console.log('Feedback Update success');
         res.send('Feedback failed to update!');
+    }
+});
+
+// Blog Routes.
+app.get('/api/blog', async (req, res) => {
+    console.log('/api/blog - GET');
+    
+    const allBlogs = await Blog.findAll();
+    res.send(allBlogs);
+});
+
+app.get('/api/blog/edit/:id', async (req, res) => {
+    console.log('/api/blog/edit/:id - GET');
+    let id = req.params.id;
+    id = parseInt(id);
+
+    const oneBlog = await Blog.findByPk(id);
+
+    if(oneBlog === null){
+        console.log('Blog Not Found!');
+        res.send('No Blog item found!');
+    } else {
+        console.log('Blog Found!');
+        res.send(oneBlog);
+    }
+});
+
+app.post('/api/blog/add', async (req, res) => {
+    console.log('/api/blog/add - POST');
+    
+    const { blogHeader, blogTitle, blogBody, UserUserId } = req.body;
+    const newBlog = await Blog.create({ blogHeader, blogTitle, blogBody, UserUserId});
+
+    console.log(newBlog.toJSON());
+    res.send(newBlog);
+});
+
+app.put('/api/blog/edit/:id', async (req, res) => {
+    console.log('/api/blog/edit/:id - PUT');
+    let id = req.params.id;
+    id = parseInt(id);
+
+    const { blogHeader, blogTitle, blogBody, UserUserId } = req.body;
+    const editBlog = await Blog.update({ blogHeader, blogTitle, blogBody, UserUserId }, {
+        where: {
+            blogId: id
+        }
+    });
+    
+    if (editBlog == 0) {
+        console.log('Blog Update Failed!');
+        res.send('Blog failed to update!');
+    } else {
+        console.log('Blog Update Success!');
+        res.send('Blog has been updated!');
+    }
+});
+
+app.delete('/api/blog/delete/:id', async (req, res) => {
+    console.log('/api/blog/delete/:id - DELETE');
+    let id = req.params.id;
+    id = parseInt(id);
+
+    const deleteBlog = await Blog.destroy({
+        where: {
+            blogId: id
+        }
+    });
+    
+    if (deleteBlog == 0) {
+        console.log('Blog delete Failed!');
+        res.send('Blog failed to delete!');
+    } else {
+        console.log('Blog delete Success!');
+        res.send('Blog has been deleted!');
     }
 });
 
