f2398e543e3beafd0966a8937980eb1985e130d5
Slebby <109596501+Slebby@users.noreply.github.com>
2023-10-28 17:51:26 +1100

    Add Fetch Feedback

--- Client/src/reducer/feedbackSlice.js
+++ Client/src/reducer/feedbackSlice.js
@@ -1,4 +1,5 @@
 import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+import axios from "axios";
 
 const initialState = {
     feedbackList: [],
@@ -6,12 +7,38 @@ const initialState = {
     error: null
 };
 
+// Fetch feedback 
+export const fetchFeedbackList = createAsyncThunk('feedback/fetchFeedback', async() => {
+    console.log('Fetching Data...');
+    try {
+        const res = await axios.get('/api/feedback');
+        console.log(res.data);
+        return res.data;
+    } catch (err) {
+        return err.message;
+    }
+})
+
 const feedbackSlice = createSlice({
     name: 'feedback',
     initialState,
     reducers: {
-
+        
+    },
+    extraReducers: (builder) => {
+        builder
+            .addCase(fetchFeedbackList.pending, (state, action) => {
+                state.status = 'loading';
+            })
+            .addCase(fetchFeedbackList.fulfilled, (state, action) => {
+                state.status = 'succeeded';
+                state.feedbackList = action.payload;
+            })
+            .addCase(fetchFeedbackList.rejected, (state, action) => {
+                state.status = 'failed';
+                state.error = action.error.message;
+            })
     }
-})
+});
 
 export default feedbackSlice.reducer;
