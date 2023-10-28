0ae002e79c140131255fdeacafe8c9559e0511ce
Slebby <109596501+Slebby@users.noreply.github.com>
2023-10-28 17:13:29 +1100

    import the slice reducer

--- Client/src/store/store.js
+++ Client/src/store/store.js
@@ -1,9 +1,13 @@
 import { configureStore } from '@reduxjs/toolkit';
+import feedbackReducer from '../reducer/feedbackSlice';
+import authReducer from '../reducer/authSlice';
+import userReducer from '../reducer/userSlice';
+import blogReducer from '../reducer/blogSlice';
 
 
 export const store = configureStore({
     reducer: {
-        auth: authReducerm,
+        auth: authReducer,
         users: userReducer,
         feedback: feedbackReducer,
         blog: blogReducer
