0fae7d220821d85a5c4e901bdd5b7c292b98c4fb
Slebby <109596501+Slebby@users.noreply.github.com>
2023-10-28 17:10:03 +1100

    Remove Consumer, axios, disptach

--- Client/src/components/feedback/AddFeedback.jsx
+++ Client/src/components/feedback/AddFeedback.jsx
@@ -22,7 +22,7 @@ const AddFeedback = (props) => {
     });
   };
 
-  const feedbackOnSubmit = async (e, dispatch) => {
+  const feedbackOnSubmit = async (e) => {
     e.preventDefault();
 
     console.log('Add feedback - Submitting Form...');
@@ -34,54 +34,46 @@ const AddFeedback = (props) => {
 
     console.log(newFeedback);
 
-    const res = await axios.post('/api/feedback/add', newFeedback);
-    dispatch({ type: 'ADD_FEEDBACK', payload: res.data });
     navigate('/feedback');
   };
 
-  return (
-    <Consumer>
-        { value => { const { dispatch } = value 
-            return (
-            <div className="container mb-5">
-                <h3 className="text-center m-4 fw-semibold">Add Feedback</h3>
-                <p>
-                    <Link className="link-dark link-underline link-underline-opacity-0 link-opacity-75-hover" to="/feedback">
-                        <FaAnglesLeft className="mb-1 me-1"/>Back
-                    </Link>
-                </p>
-                <section className="card shadow secondary-bg-color border-0">
-                    <form onSubmit={e => feedbackOnSubmit(e, dispatch)}>
-                        <div className="card-body">
-                            <div className="card-title">
-                                <div className="form-floating">
-                                    <input type="text" name="feedbackTitle" id="floatingTitle" placeholder="Title Text Here" className="form-control" value={feedbackTitle}
-                                    onChange={e => feedbackOnChange(e)} />
-                                    <label htmlFor="floatingTitle" className="opacity-75">Title</label>
-                                </div>
-                            </div>
-                            <div className="card-text">
-                                <div className="form-floating">
-                                    <textarea
-                                    type="text"
-                                    name="feedbackBody"
-                                    id="floatingText"
-                                    placeholder="Body Text Here"
-                                    className="form-control"
-                                    style={{height: "20rem"}} value={feedbackBody}
-                                    onChange={e => feedbackOnChange(e)} />
-                                    <label htmlFor="floatingText" className="opacity-75">Body</label>
-                                </div>
-                            </div>
-                            <button type="submit" value="Post Feedback" className="btn btn-lg main-bg-color w-100 btn-color text-light mt-3">Submit</button>
+    return (
+    <div className="container mb-5">
+        <h3 className="text-center m-4 fw-semibold">Add Feedback</h3>
+        <p>
+            <Link className="link-dark link-underline link-underline-opacity-0 link-opacity-75-hover" to="/feedback">
+                <FaAnglesLeft className="mb-1 me-1"/>Back
+            </Link>
+        </p>
+        <section className="card shadow secondary-bg-color border-0">
+            <form onSubmit={e => feedbackOnSubmit(e)}>
+                <div className="card-body">
+                    <div className="card-title">
+                        <div className="form-floating">
+                            <input type="text" name="feedbackTitle" id="floatingTitle" placeholder="Title Text Here" className="form-control" value={feedbackTitle}
+                            onChange={e => feedbackOnChange(e)} />
+                            <label htmlFor="floatingTitle" className="opacity-75">Title</label>
                         </div>
-                    </form>
-                </section>
-            </div>
-            )
-        }}
-    </Consumer>
-  )
+                    </div>
+                    <div className="card-text">
+                        <div className="form-floating">
+                            <textarea
+                            type="text"
+                            name="feedbackBody"
+                            id="floatingText"
+                            placeholder="Body Text Here"
+                            className="form-control"
+                            style={{height: "20rem"}} value={feedbackBody}
+                            onChange={e => feedbackOnChange(e)} />
+                            <label htmlFor="floatingText" className="opacity-75">Body</label>
+                        </div>
+                    </div>
+                    <button type="submit" value="Post Feedback" className="btn btn-lg main-bg-color w-100 btn-color text-light mt-3">Submit</button>
+                </div>
+            </form>
+        </section>
+    </div>
+    )
 }
 
 export default AddFeedback
--- Client/src/components/feedback/Feedback.jsx
+++ Client/src/components/feedback/Feedback.jsx
@@ -6,30 +6,24 @@ import { Consumer } from '../../context/context';
 
 const Feedback = (props) => {
 
-    return(
-        <Consumer>
-            { value => { const {feedbackList} = value; 
-                return (
-                <main className="mb-5">
-                    <div className="container text-center">
-                        <h3 className="m-4 fw-semibold">Feedback List</h3>
-                        <p className="text-start mx-4">
-                            <Link className="link-dark link-underline link-underline-opacity-0 link-opacity-75-hover" to="./add">
-                                <FaPlus className="mb-1 me-1"/>Add
-                            </Link>
-                        </p>
-                        <div className="row row-cols-2 gap-5 justify-content-center">
-                            {
-                                feedbackList.map( feedback => (
-                                    <SingleFeedback key={feedback.feedId} feedback={feedback} />
-                                ))
-                            }
-                        </div>
-                    </div>
-                </main>
-                )
-            }}
-        </Consumer>
+    return (
+    <main className="mb-5">
+        <div className="container text-center">
+            <h3 className="m-4 fw-semibold">Feedback List</h3>
+            <p className="text-start mx-4">
+                <Link className="link-dark link-underline link-underline-opacity-0 link-opacity-75-hover" to="./add">
+                    <FaPlus className="mb-1 me-1"/>Add
+                </Link>
+            </p>
+            <div className="row row-cols-2 gap-5 justify-content-center">
+                {
+                    feedbackList.map( feedback => (
+                        <SingleFeedback key={feedback.feedId} feedback={feedback} />
+                    ))
+                }
+            </div>
+        </div>
+    </main>
     )
 }
 
