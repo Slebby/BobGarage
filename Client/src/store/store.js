7783f9cb534e235deb88a2d6901f3f101fc5a11e
Slebby <109596501+Slebby@users.noreply.github.com>
2023-10-29 16:12:24 +1100

    Comment out the users and add car_services

--- Client/src/store/store.js
+++ Client/src/store/store.js
@@ -1,15 +1,17 @@
 import { configureStore } from '@reduxjs/toolkit';
 import feedbackReducer from '../reducer/feedbackSlice';
 import authReducer from '../reducer/authSlice';
-import userReducer from '../reducer/userSlice';
+// import userReducer from '../reducer/userSlice';
 import blogReducer from '../reducer/blogSlice';
+import carServiceReducer from '../reducer/carServiceSlice';
 
 
 export const store = configureStore({
     reducer: {
         auth: authReducer,
-        users: userReducer,
+        // users: userReducer,
         feedback: feedbackReducer,
-        blog: blogReducer
+        blog: blogReducer,
+        car_Service: carServiceReducer
     }
 });
