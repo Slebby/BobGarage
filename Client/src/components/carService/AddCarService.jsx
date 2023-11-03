a1158d71ef40b39dd1b5052c387a39317f7fb428
Slebby <109596501+Slebby@users.noreply.github.com>
2023-11-03 15:27:19 +1100

    Add price input and fix the number input

--- Client/src/components/carService/AddCarService.jsx
+++ Client/src/components/carService/AddCarService.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import { useState, useEffect } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
 import { FaAnglesLeft } from 'react-icons/fa6';
 import { useDispatch } from 'react-redux';
@@ -10,10 +10,12 @@ const AddCarService = () => {
     serviceName: '',
     serviceDesc: '',
     serviceImage: null,
+    servicePrice: 0,
+    servicePriceDecimal: 0,
     errors: {}
   });
 
-  const { serviceName, serviceDesc, serviceImage, errors } = formData;
+  const { serviceName, serviceDesc, serviceImage, servicePrice, servicePriceDecimal, errors } = formData;
   const navigate = useNavigate();
 
   const serviceOnChange = e => {
@@ -24,6 +26,39 @@ const AddCarService = () => {
     });
   };
 
+  // Handle Number Input
+  const handleKeyPress = (e) => {
+    // Allow number only
+    const regex = /^[A-Za-z]$/;
+    const keyCode = e.key;
+    const disabledKeys = ['.', '-', '+', 'ArrowUp', 'ArrowDown'];
+
+    if (regex.test(keyCode) || disabledKeys.includes(keyCode)) {
+      e.preventDefault();
+    }
+  };
+
+  const numberOnChange = (e, digit) => {
+    if (e.target.value.length <= digit ){
+        setFormData({
+            ...formData, [e.target.name]: e.target.value
+        });
+    }
+  }
+
+  const numberOnScroll = e => {
+    e.preventDefault();
+  }
+
+  useEffect(() => {
+    const inputElement = document.querySelector('input[type="number"]');
+    inputElement.addEventListener('wheel', numberOnScroll, { passive: false });
+
+    return () => {
+      inputElement.removeEventListener('wheel', numberOnScroll);
+    };
+  }, []);
+
   const serviceOnSubmit = async (e) => {
     e.preventDefault();
 
@@ -32,8 +67,11 @@ const AddCarService = () => {
     const newService = {
         serviceName,
         serviceDesc,
-        serviceImage
+        serviceImage,
+        servicePrice: parseFloat(`${servicePrice}.${servicePriceDecimal}`)
     }
+
+    // console.log(newService);
 
     dispatch(addNewCarService(newService));
 
@@ -66,9 +104,36 @@ const AddCarService = () => {
                             id="floatingText"
                             placeholder="Desc Text Here"
                             className="form-control"
-                            style={{height: "20rem"}} value={serviceDesc}
-                            onChange={e => serviceOnChange(e)} />
+                            style={{height: "20rem"}}
+                            value={serviceDesc}
+                            onChange={e => serviceOnChange(e)}
+                            inputMode="numeric"
+                            pattern="[0-9]*"/>
                             <label htmlFor="floatingText" className="opacity-75">Description</label>
+                        </div>
+                    </div>
+                    <div className="input-group card-subtitle my-1 w-50 container">
+                        <span className="input-group-text fs-4">&#x0024;</span>
+                        <div className="form-floating w-10">
+                            <input
+                            type="number"
+                            name="servicePrice"
+                            id="servicePrice"
+                            placeholder="Number"
+                            className="form-control"
+                            value={servicePrice ? servicePrice : ''}
+                            onChange={e => numberOnChange(e, 10)}
+                            onKeyDown={e => handleKeyPress(e)}
+                            inputMode="numeric"
+                            pattern="[0-9]*"/>
+                            <label htmlFor="servicePrice" className="opacity-75">Price</label>
+                        </div>
+                        <span className="input-group-text fs-4">&#x002E;</span>
+                        <div className="form-floating">
+                            <input type="number" name="servicePriceDecimal" id="servicePriceDecimal" placeholder="00" className="form-control" value={servicePriceDecimal ? servicePriceDecimal : ''}
+                            onKeyDown={e => handleKeyPress(e)}
+                            onChange={e => numberOnChange(e, 2)}/>
+                            <label htmlFor="servicePriceDecimal" className="opacity-75">Decimal &#x0028;00&#x0029;</label>
                         </div>
                     </div>
                     <button type="submit" value="Post Feedback" className="btn btn-lg main-bg-color w-100 btn-color text-light mt-3">Add</button>
