5fe828bbf52608cdabbb40fce90eb2b054ce8124
Slebby <109596501+Slebby@users.noreply.github.com>
2023-11-03 13:43:18 +1100

    Add the price to the service

--- Client/src/components/carService/SingleCarService.jsx
+++ Client/src/components/carService/SingleCarService.jsx
@@ -6,7 +6,7 @@ import { removeCarService } from '../../reducer/carServiceSlice';
 
 const SingleCarService = ({carService}) => {
     const dispatch = useDispatch();
-  const { serviceId, serviceName, serviceDesc, serviceImage } = carService;
+  const { serviceId, serviceName, serviceDesc, serviceImage, servicePrice } = carService;
   const serviceOnDelete = (id) => {
     console.log('Delete Clicked!');
     console.log(`ID: ${id}`);
@@ -21,12 +21,15 @@ const SingleCarService = ({carService}) => {
         <div className="card shadow col-md-5 px-0">
             <img src={`${serviceImage !== null ? serviceImage : '#'}`} alt="Picture" className="card-img-top"/>
             <div className="card-body">
-                <h5 className="card-title border-bottom pb-2">
+                <h3 className="card-title border-bottom pb-2">
                     {serviceName}
-                    </h5>
+                </h3>
                 <p className="card-text">
                     {serviceDesc}
                 </p>
+                <h4 className="card-subtitle mb-3">
+                    Price: &#x0024;{servicePrice.toFixed(2)}
+                </h4>
                 <Link className="btn main-bg-color btn-color me-3 fw-semibold text-light" to={`./edit/${serviceId}`}>
                     <FaPen className="me-2 mb-1"/>Edit
                 </Link>
