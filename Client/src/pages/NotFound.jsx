7560cde0bd89a0f14436f38c9daf6e70f88be8c8
Slebby <109596501+Slebby@users.noreply.github.com>
2023-11-17 11:22:27 +1100

    Customize the NotFound page

--- Client/src/pages/NotFound.jsx
+++ Client/src/pages/NotFound.jsx
@@ -1,15 +1,16 @@
 import { Link } from 'react-router-dom';
-import { FaExclamation } from 'react-icons/fa6';
+import { BsExclamationTriangle } from "react-icons/bs";
 
 const NotFound = () => {
   return (
-    <section className="container">
+    <section className="container text-center my-5">
         <h1 className="display-3">
-            <span className="text-danger">
-                <FaExclamation />
-            </span>{' '}Oops... Page not found
-            <p className="lead">This URL is not exist!</p>
-            <Link to="/">Return to home page</Link>
+            <span className="text-danger d-block">
+                <BsExclamationTriangle className="w-50 h-auto"/>
+            </span>
+            <span>Oops... Page not found</span>
+            <p className="fs-1 ">This URL is not exist!</p>
+            <Link to="/" className="link-dark link-underline-dark link-underline-opacity-50 link-opacity-75-hover fw-semibold">Return to home page</Link>
         </h1>
     </section>
   )
