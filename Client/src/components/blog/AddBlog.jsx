89746b8b4e4c845e9bc930361595194830146b3a
Slebby <109596501+Slebby@users.noreply.github.com>
2023-11-22 04:30:11 +1100

    change the oprator

--- Client/src/components/blog/AddBlog.jsx
+++ Client/src/components/blog/AddBlog.jsx
@@ -127,7 +127,7 @@ const AddBlog = (props) => {
   
   return (
     <div className="container mb-5">
-      {pageIsLoading || (
+      {pageIsLoading && (
         <div className="d-flex justify-content-center align-items-center text-light" id="overlay">
           <span className="text-center fs-3 fw-semibold me-3 mb-2" id="loadingOpacity">Posting</span>
           <div className="spinner-border" role="status">
