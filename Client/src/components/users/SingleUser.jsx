2cd3d6ab0462a46ac052408ca8ea7e209bcae3ed
Slebby <109596501+Slebby@users.noreply.github.com>
2023-11-24 09:31:41 +1100

    move the loading

--- Client/src/components/users/SingleUser.jsx
+++ Client/src/components/users/SingleUser.jsx
@@ -33,26 +33,26 @@ const SingleUser = ({ user }) => {
   }
   return (
     <tr>
-        {pageIsLoading && (
-          <tr>
-            <Spinner loadingLabel="Deleting" />
-          </tr>
-        )}
         <td>{user.userId}</td>
         <td>{user.username}</td>
         <td className="text-lowercase">{user.email}</td>
         <td>{user.isStaff ? 'Yes' : 'No'}</td>
         <td>{user.userImage ? (
           <img src={user.userImage} alt={`name: ${user.username}`} className="h-auto" style={{width: "5rem"}}/>
-        ) : (
-          <img src="defaultPfp/profile-default-dark.svg" alt="defaultPfp" className="h-auto rounded-circle" style={{width: "5rem"}}/>
-          )}
+          ) : (
+            <img src="defaultPfp/profile-default-dark.svg" alt="defaultPfp" className="h-auto rounded-circle" style={{width: "5rem"}}/>
+            )}
         </td>
         <td>
             <Link className="btn btn-danger ms-3 fw-semibold" onClick={() => {userOnDelete(user.userId)}}>
               <FaTrashCan className="me-2 mb-1"/>Delete
             </Link>
         </td>
+        {pageIsLoading && (
+          <td>
+            <Spinner loadingLabel="Deleting" />
+          </td>
+        )}
     </tr>
   )
 }
