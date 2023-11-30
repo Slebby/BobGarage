c2b3e2d551b8a0d11664342a17fe7a1b1e8b114d
Slebby <109596501+Slebby@users.noreply.github.com>
2023-12-01 10:45:55 +1100

    Make it show the icons when them changes

--- Client/src/components/layout/Switch.jsx
+++ Client/src/components/layout/Switch.jsx
@@ -1,23 +1,38 @@
-import { Fragment } from 'react';
+import { useEffect, useState } from 'react';
 import { IoMoon, IoSunny } from "react-icons/io5";
 
 const Switch = () => {
+  const [darkThemeIcon, setDarkThemeIcon] = useState(false);
   const setDarkMode = () => {
     document.querySelector("body").setAttribute("data-theme", "dark");
+    localStorage.setItem("selectedTheme", "dark");
+    setDarkThemeIcon(true);
   }
 
   const setLightMode = () => {
     document.querySelector("body").setAttribute("data-theme", "light");
+    localStorage.setItem("selectedTheme", "light");
+    setDarkThemeIcon(false);
   }
+  const selectedTheme = localStorage.getItem("selectedTheme");
+
+  useEffect(() => {
+    if (selectedTheme === "dark") {
+      setDarkMode();
+    }
+  }, [])
 
   const toggleTheme = e => {
-    if (e.target.checked) setLightMode();
-    else setDarkMode();
+    if (e.target.checked) {
+        setDarkMode();
+    } else {
+        setLightMode();
+    }
   }
   return (
     <div className="form-check form-switch">
-        <input class="form-check-input" type="checkbox" role="switch" id="flexSwitchCheckDefault" onChange={e => toggleTheme(e)}/>
-        <label class="form-check-label text-light" for="flexSwitchCheckDefault">Theme</label>
+        <input class="form-check-input" type="checkbox" role="switch" id="flexSwitchCheckDefault" onChange={e => toggleTheme(e)} defaultChecked={selectedTheme === "dark"}/>
+        <label class="form-check-label text-light" for="flexSwitchCheckDefault">{darkThemeIcon ? (<IoMoon className="mb-1"/>) : (<IoSunny className="mb-1"/>)}</label>
     </div>
   )
 }
