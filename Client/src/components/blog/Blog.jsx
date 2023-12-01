db514b32694e5cc13b6a9f7be70b2675bc43ae3b
Slebby <109596501+Slebby@users.noreply.github.com>
2023-12-01 15:29:37 +1100

    Make a sort for blog

--- Client/src/components/blog/Blog.jsx
+++ Client/src/components/blog/Blog.jsx
@@ -1,4 +1,4 @@
-import { Fragment } from 'react'
+import { Fragment, useState } from 'react'
 import PropTypes from 'prop-types'
 import { Link, useLocation } from 'react-router-dom';
 import { FaMicroblog } from 'react-icons/fa6';
@@ -18,6 +18,25 @@ const Blog = props => {
     const userNameLists = useSelector(selectAll_User);
 
     const homePath = pathLocation === '/';
+
+    let blogs = [...blogList];
+    const [sortOption, setSortOption] = useState("relevance");
+    
+    if(sortOption === "oldest") {
+        blogs.sort((a, b) => {
+            let dateA = new Date(a.createdAt),
+            dateB = new Date(b.createdAt);
+            return dateB - dateA;
+        });
+    } else if(sortOption === "newest") {
+        blogs.sort((a, b) => {
+            let dateA = new Date(a.createdAt),
+            dateB = new Date(b.createdAt);
+            return dateA - dateB;
+        });
+    } else if(sortOption === "relevance") {
+        blogs = blogList;
+    }
 
     let content;
     if(blogStatus === 'loading'){
@@ -44,15 +63,18 @@ const Blog = props => {
                                 </Link>
                             </span>
                             <div className="dropdown float-end">
-                                <button className="btn main-bg-color btn-color text-light dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false">
-                                    Sort By
+                                <button className="btn main-bg-color btn-color text-light dropdown-toggle text-capitalize" type="button" data-bs-toggle="dropdown" aria-expanded="false">
+                                    Sort By {sortOption}
                                 </button>
                                 <ul className="dropdown-menu">
                                     <li>
-                                        <Link className="dropdown-item">Newest</Link>
+                                        <Link className="dropdown-item" onClick={e => setSortOption("relevance")}>Relevance</Link>
                                     </li>
                                     <li>
-                                        <Link className="dropdown-item">Oldest</Link>
+                                        <Link className="dropdown-item" onClick={e => setSortOption("newest")}>Newest</Link>
+                                    </li>
+                                    <li>
+                                        <Link className="dropdown-item" onClick={e => setSortOption("oldest")}>Oldest</Link>
                                     </li>
                                 </ul>
                             </div>
@@ -60,7 +82,7 @@ const Blog = props => {
                     ) : null}
                     <div className="row row-cols-2 gap-5 justify-content-center">
                         {
-                            blogList.map(
+                            blogs.map(
                                 item => {
                                 let user = userNameLists.filter( u => u.userId === item.myUserBlogId);
                                 return <SingleBlog key={ item.blogId } blog={item} user={user}/>}
