b188c5408bd206f6cb4f0ea1512f6a1e07e7e14f
Slebby <109596501+Slebby@users.noreply.github.com>
2024-01-07 20:14:46 +1100

    Add some icons to it

--- Client/src/components/auth/emailVerify/VerifyEmail.jsx
+++ Client/src/components/auth/emailVerify/VerifyEmail.jsx
@@ -3,13 +3,14 @@ import axios from "axios";
 import { useLocation, Link, Navigate } from "react-router-dom";
 import { useDispatch, useSelector } from "react-redux";
 import { verifyEmail, getAuthUser, getError, logout } from "../../../reducer/authSlice";
+import { RiMailCloseLine, RiMailCheckLine, RiErrorWarningFill } from "react-icons/ri";
 
 const VerifyEmail = () => {
   const dispatch = useDispatch();
   const location = useLocation();
   const queryParams = new URLSearchParams(location.search);
   const token = queryParams.get('token');
-  const fromLoginPage = location.pathname;
+  const fromLoginPage = '/login/verify';
   const user = useSelector(getAuthUser);
   const isError = useSelector(getError);
   const [verificationStatus, setVerificationStatus] = useState('');
@@ -58,14 +59,20 @@ const VerifyEmail = () => {
     <section className='container my-5'>
       {!token && !fromLoginPage && (
         <Fragment>
-            <p>
-              Thanks for signing up with Bob's Garage! To activate your account, please check your email and click the verification link we sent you. If you don't see the email, check your spam folder or click 'Resend Verification' below. Need assistance? Contact us at <Link to={`mailto:${supportEmail}`}>{supportEmail}</Link>.
-            </p>
-            {verificationButton}
+          <div className="text-center">
+            <RiMailCheckLine className="h-auto w-50 text-success"/>
+          </div>
+          <p>
+            Thanks for signing up with Bob's Garage! To activate your account, please check your email and click the verification link we sent you. If you don't see the email, check your spam folder or click 'Resend Verification' below. Need assistance? Contact us at <Link to={`mailto:${supportEmail}`}>{supportEmail}</Link>.
+          </p>
+          {verificationButton}
         </Fragment>      
       )}
       {token && !isError && (
         <Fragment>
+          <div className="text-center">
+            <RiMailCheckLine className="h-auto w-50 text-success"/>
+          </div>
           <p>
             Congratulations! Your email has been successfully verified, and your account with Bob's Garage is now active. Welcome to our community of automotive enthusiasts! Feel free to explore our services and don't hesitate to reach out if you have any questions.
           </p>
@@ -80,8 +87,11 @@ const VerifyEmail = () => {
           </div>
         </Fragment>
       )}
-      {!token && fromLoginPage && (
+      {!token && fromLoginPage === location.pathname && (
         <Fragment>
+          <div className="text-center">
+            <RiMailCloseLine className="h-auto w-50 text-warning"/>
+          </div>
           <p>
             Oops! It looks like your email hasn't been verified yet. To complete your registration and access your account, please check your email for a verification link. If you can't find it, click 'Resend Verification' below. Need help? Contact us at <Link to={`mailto:${supportEmail}`}>{supportEmail}</Link>.
           </p>
@@ -90,6 +100,9 @@ const VerifyEmail = () => {
       )}
       {isError === "Invalid Verification Link" && (
         <Fragment>
+          <div className="text-center">
+            <RiErrorWarningFill className="h-auto w-50 text-danger"/>
+          </div>
           <p>
             We're sorry, but it seems the verification link may have expired or is invalid. To ensure access to our services, please request a new verification email by clicking "Resend Verification" below. Check your email and complete the verification process.
             </p>
