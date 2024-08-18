916886077ae9a1416ff3b1cda257bedfd6af2526
Slebby <109596501+Slebby@users.noreply.github.com>
2024-08-18 16:03:39 +1000

    Update vite.config.js

--- Client/vite.config.js
+++ Client/vite.config.js
@@ -3,12 +3,16 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/",
   plugins: [react()],
   server: {
     proxy: {
       '/api': {
         target: 'http://localhost:3060'
       }
-    }
+    },
+    port:3000,
+    strictPort: true,
+    host: true,
   }
 })
